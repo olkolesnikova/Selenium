3cc4933b4d746db93ad22aeb57b5f6a076db34cc
olkolesnikova <59109598+olkolesnikova@users.noreply.github.com>
2020-05-26 16:46:00 +0500

    Задание 1 - тест на открытие браузера

--- src/test/java/ru/stqa/OpenSite.java
+++ src/test/java/ru/stqa/OpenSite.java
@@ -2,36 +2,16 @@ package ru.stqa;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
-public class OpenSite {
-    private WebDriver driver;
-    private String baseUrl;
-
-    @BeforeClass(alwaysRun = true)
-    public void setUp() throws Exception {
-        driver = new FirefoxDriver();
-        baseUrl = "https://www.google.com/";
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    }
+public class OpenSite extends TestBase {
 
     @Test
-    public void testUntitledTestCase() throws Exception {
+    public void testOpenSite() throws Exception {
         driver.get("https://www.google.com/");
         driver.findElement(By.name("q")).clear();
         driver.findElement(By.name("q")).sendKeys("selenium webdriver");
         driver.findElement(By.id("tsf")).submit();
-    }
-
-    @AfterClass(alwaysRun = true)
-    public void tearDown() throws Exception {
-        driver.quit();
-
     }
 
 }
---
+++ src/test/java/ru/stqa/TestBase.java
@@ -0,0 +1,24 @@
+package ru.stqa;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+
+    public WebDriver driver;
+
+    @BeforeClass(alwaysRun = true)
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+}
